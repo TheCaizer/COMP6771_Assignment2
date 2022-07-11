e2a7e35c175c2a56de6ecb59d7f70b90b58aa31e
Jackie Cai <z5259449@ad.unsw.edu.au>
2022-07-11 20:35:15 +1000

    done

--- test/euclidean_vector/euclidean_vector_test_utility.cpp
+++ test/euclidean_vector/euclidean_vector_test_utility.cpp
@@ -111,3 +111,18 @@ TEST_CASE("Dot") {
 		CHECK(comp6771::dot(a, b) == Approx(24.8));
 	}
 }
+
+TEST_CASE("Creation of unit vectors") {
+	SECTION("You have two identical vectors") {
+		auto a = comp6771::euclidean_vector(2);
+		a[0] = 1;
+		a[1] = 2;
+		auto b = comp6771::euclidean_vector(2);
+		b[0] = 1;
+		b[1] = 2;
+
+		auto c = comp6771::unit(a);
+		auto d = comp6771::unit(b);
+		REQUIRE(c == d);
+	}
+}
